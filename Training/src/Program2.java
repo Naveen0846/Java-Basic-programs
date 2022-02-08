9f536a54968debedbdaba58d97e2dcb59bce9783
Naveen0846 <62131417+Naveen0846@users.noreply.github.com>
2022-02-08 13:55:17 +0530

    Add files via upload

---
+++ Training/src/Program2.java
@@ -0,0 +1,21 @@
+
+class Add{
+	int a, b;
+	
+	void  add() {
+	}
+	int Add(int x, int y) {
+		a =x;
+		b =y;
+		return a+b;
+	}
+	
+}
+
+public class Program2 {
+	public static void main(String[] args) {
+		Add ad =new Add();
+		int add = ad.Add(10, 20);
+		System.out.println(add);
+	}
+}
